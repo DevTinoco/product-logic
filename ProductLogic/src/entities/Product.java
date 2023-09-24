ee4de426fbd567caec16484c28ce664a027b039c
Valdinei Tinoco <valdinei_ct@hotmail.com>
2023-09-23 22:57:25 -0300

    Product

--- ProductLogic/src/entities/Product.java
+++ ProductLogic/src/entities/Product.java
@@ -1,5 +1,11 @@
-package entities;
+package entities; // USAMOS ENTITIAS, POIS ELA É UMA ENTIDADE DO MEU NEGOCIO.
 
 public class Product {
 
+	public String name;
+	public double price;
+	public int quantit;
+	
+	
+	
 }
